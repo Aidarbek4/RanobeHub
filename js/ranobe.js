795ec9b1b17a85bb0a69801b731cf7ebe177d473
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-04-06 08:29:05 +0600

    ranobe pahe

---
+++ js/ranobe.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).className += " active";
+    evt.currentTarget.className += " active";
+  }
